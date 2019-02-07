fadc78b5e53e832598e4b3170b20ca0e090c4355
Dawid Nowogorski <nowogorski.dawid@gmail.com>
2019-02-07 14:45:17 +0100

    Function composition

--- src/test/java/org/dnowogorski/FunctionsTest.java
+++ src/test/java/org/dnowogorski/FunctionsTest.java
@@ -1,5 +1,6 @@
 package org.dnowogorski;
 
+import io.vavr.Function1;
 import io.vavr.Function2;
 import io.vavr.Function3;
 import org.junit.Test;
@@ -8,6 +9,9 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FunctionsTest {
+
+    private static final Function1<Integer, Integer> INCREMENT = a -> a + 1;
+    private static final Function1<Integer, Integer> MULTIPLY_BY_TWO = a -> a * 2;
 
     @Test
     public void shouldCreateFunction2() {
@@ -21,6 +25,23 @@ public class FunctionsTest {
         Function3<String, String, String, String> url = Function3.of(this::createUrl);
 
         assertThat(url.apply("https://github.com", "dnowogorski", "vavr-playground"), is("https://github.com/dnowogorski/vavr-playground"));
+    }
+
+    @Test
+    public void shouldComposeFunctions() {
+        // First increment then multiply by two
+        Function1<Integer, Integer> composed = INCREMENT.andThen(MULTIPLY_BY_TWO);
+
+        assertThat(composed.apply(2), is(6));
+    }
+
+    @Test
+    public void shouldComposeFunctionsV2() {
+        Function1<Integer, Integer> decrement = a -> a - 1;
+        // First increment then multiply by two then decrement
+        Function1<Integer, Integer> composed = decrement.compose(MULTIPLY_BY_TWO.compose(INCREMENT));
+
+        assertThat(composed.apply(3), is(7));
     }
 
     private String createUrl(String base, String user, String repo) {
