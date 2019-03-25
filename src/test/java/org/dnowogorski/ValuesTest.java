bed321c7d0c74a031750e32b49cf36ad77e75551
Dawid Nowogorski <nowogorski.dawid@gmail.com>
2019-03-25 22:16:26 +0100

    Play with Try and pattern matching

--- src/test/java/org/dnowogorski/ValuesTest.java
+++ src/test/java/org/dnowogorski/ValuesTest.java
@@ -1,8 +1,13 @@
 package org.dnowogorski;
 
+import io.vavr.API;
 import io.vavr.control.Option;
+import io.vavr.control.Try;
 import org.junit.Test;
 
+import static io.vavr.API.$;
+import static io.vavr.API.Case;
+import static io.vavr.Predicates.instanceOf;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
@@ -25,5 +30,28 @@ public class ValuesTest {
                 .map(String::toLowerCase)
                 .isEmpty()
         ).isEqualTo(true);
+    }
+
+    @Test
+    public void shouldTry() {
+        String somethingElse = "Some other value";
+        assertThat(Try.of(this::bunchOfWork).getOrElse(somethingElse)).isEqualTo(somethingElse);
+    }
+
+    @Test
+    public void shouldTryAndRecover() {
+        String result = Try.of(this::bunchOfWork).recover(x -> API.Match(x).of(
+                Case($(instanceOf(RuntimeException.class)), this::handleException)
+        )).get();
+
+        assertThat(result).isEqualTo("Something bad happened...");
+    }
+
+    private String bunchOfWork() {
+        throw new RuntimeException("Something bad happened...");
+    }
+
+    private String handleException(RuntimeException ex) {
+        return ex.getMessage();
     }
 }
